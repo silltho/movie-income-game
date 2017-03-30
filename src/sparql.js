6f038c46ac6ac51f4b704b0b205539d395348d0b
Christoph Karl Mayr <cmayr.mmt-b2015@fh-salzburg.ac.at>
2017-03-30 18:23:51 +0200

    refactoring2

--- src/sparql.js
+++ src/sparql.js
@@ -16,17 +16,10 @@ const sparql = (function() {
   } LIMIT 1000 OFFSET 0
 */
 
-  const baseUrl = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=';
+  const moviesQuery = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0A%0D%0ASELECT+DISTINCT+%3Fid+%3Fruntime+%3Fgross+%3Fbudget+%3Fname%0D%0AWHERE+%7B%0D%0A%3Fid+rdf%3Atype+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2FFilm%3E+%3B%0D%0A+++dbo%3Aruntime+%3Fruntime+%3B%0D%0A+++dbo%3Agross+%3Fgross+%3B%0D%0A+++dbp%3Abudget+%3Fbudget+%3B%0D%0A+++dbp%3Aname+%3Fname%0D%0A%7D+LIMIT+1000+OFFSET+0&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30&debug=on';
 
-  const buildMoviesQuery = (id) => {
-    const mqp1 = 'http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=PREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0A%0D%0ASELECT+DISTINCT+',
-          mqp2 = '+%3Fruntime+%3Fgross+%3Fbudget+%3Fname%0D%0AWHERE+%7B%0D%0A%3Fid+rdf%3Atype+%3Chttp%3A%2F%2Fdbpedia.org%2Fontology%2FFilm%3E+%3B%0D%0A+++dbo%3Aruntime+%3Fruntime+%3B%0D%0A+++dbo%3Agross+%3Fgross+%3B%0D%0A+++dbp%3Abudget+%3Fbudget+%3B%0D%0A+++dbp%3Aname+%3Fname%0D%0A%7D+LIMIT+1000+OFFSET+0&format=application%2Fsparql-results%2Bjson&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30&debug=on',
-          title_placeholder = '%3Fid';
-    if (id) {
-      return mqp1 + encodeURI(id) + mqp1;
-    }
-
-    return mqp1 + title_placeholder + mqp2;
+  const buildMoviesQuery = () => {
+    return moviesQuery;
   }
 
 
@@ -53,13 +46,8 @@ const sparql = (function() {
   }
 
   return {
-    queries: {
-      buildMoviesQuery,
-    },
-    parser: {
-      parseMovies
-    },
-    baseUrl,
+    buildMoviesQuery,
+    parseMovies,
   }
 })();
 
