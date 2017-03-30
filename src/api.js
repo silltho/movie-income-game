d2885625933e4378bdff50c919984eedc55efdda
Erfan Ebrahimnia <eebrahimnia.mmt-b2015@fh-salzburg.ac.at>
2017-03-30 17:19:34 +0200

    added api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,28 @@
 import $ from 'jquery';
+import sparql from './sparql';
 
 let url = 'https://dbpedia.org/sparql';
 let query = '';
 let format = 'json';
+
+
+export default {
+    movieList: [],
+
+    load(url) {
+        return window.fetch(url)
+            .then((res) => res.json());
+    },
+
+    loadMovieList() {
+        
+    },
+
+    getRandomMovie(movieId) {
+        // if idx is the same call rnd again
+    },
+
+    loadMoviePair(movieAId, movieBId) {
+
+    }
+};
