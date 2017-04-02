38267079aa9614ec349510f8c3e921824e9ca3eb
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2017-04-02 13:41:55 +0200

    export partial/helper loading into module

--- src/app.js
+++ src/app.js
@@ -1,25 +1,10 @@
-import page from 'page';
 import game from './game';
-import Handlebars from 'hbsfy/runtime';
 import sparql from './sparql';
+import * as helpers from './hbsHelper';
 
 // Testing
 fetch(sparql.buildMoviesQuery()).then(res => res.json()).then(sparql.parseMovies).then(console.log);
 
-// Templates
-import homeTpl from './templates/home.hbs';
-import gameSidePtl from './templates/gameSide.partial.hbs';
+helpers.registerPartials();
 
 game.init();
-
-// Partials
-Handlebars.registerPartial('gameSidePtl', gameSidePtl);
-
-// Functions
-function render(tpl, data) {
-    $app.html(tpl(data));
-}
-
-// Routing
-page('/', render(homeTpl, dummyObj));
-page();
--- src/pages.js
+++ src/pages.js
@@ -1,7 +1,9 @@
 import mainTpl from './templates/main.hbs';
+import gameSideTpl from './templates/gameSide.partial.hbs';
 
 const pages = {
     mainTpl: mainTpl,
+    gameSideTpl: gameSideTpl
 };
 
 Object.freeze(pages);
