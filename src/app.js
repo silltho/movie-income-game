e6f4c5b8ee3a969d062f7ddfaf6618d5d94d091b
Thomas Siller <tsiller.mmt-b2015@fh-salzburg.ac.at>
2017-04-04 13:30:49 +0200

    refactoring frontend

--- src/app.js
+++ src/app.js
@@ -1,10 +1,3 @@
 import game from './game';
-import sparql from './sparql';
-import * as helpers from './hbsHelper';
-
-// Testing
-fetch(sparql.buildMoviesQuery()).then(res => res.json()).then(sparql.parseMovies).then(console.log);
-
-helpers.registerPartials();
 
 game.init();
